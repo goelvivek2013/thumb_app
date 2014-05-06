0ea1e5dbdd5028fe2d051a90b1be391c95a62dde
jony <jony@idifysolutions.com>
2014-05-06 19:06:34 +0530

    count up down vote saperatly

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,13 @@ class Product < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
   has_reputation :votes, source: :user, aggregated_by: :sum
+
+	def count_positive
+	  evaluations.where( target_type: self.class, target_id: self.id, value: 1.0 ).count
+	end
+
+	def count_negative
+	  evaluations.where( target_type: self.class, target_id: self.id, value: -1.0 ).count
+	end
+
 end
