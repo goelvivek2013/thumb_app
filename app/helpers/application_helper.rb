dd0a21645b1a42c0bd90a3a2bd860e6e84eb167d
jony <jony@idifysolutions.com>
2014-05-06 14:06:04 +0530

    third commit to choose highest votted user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def self.highrankuser
+    user = User.all.map {|user| reputation = user.reputation_for(:votes).to_i , id = user.id }
+    user.sort_by{|x,y| x}.reverse!
+    winner = user.first
+    winner = User.find_by_id(winner[1])
+    a = winner.reputation_for(:votes).to_s
+    winner.email + (a)
+  end
 end
